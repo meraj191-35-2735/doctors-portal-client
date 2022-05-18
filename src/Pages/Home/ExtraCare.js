317ec4a900438806caed9d93eb5fb56faf7be1bd
meraj191-35-2735 <www.shamsad35-2735@diu.edu.bd>
2022-05-18 13:41:14 +0600

    extra care done

---
+++ src/Pages/Home/ExtraCare.js
@@ -0,0 +1,31 @@
+import React from "react";
+import treatment from "../../assets/images/treatment.png";
+
+const ExtraCare = () => {
+  return (
+    <div class="hero min-h-screen ">
+      <div class="hero-content flex-col lg:flex-row">
+        <img className="w-full lg:w-1/2 mr-auto lg:mr-10" src={treatment} />
+        <div className="ml-10">
+          <h1 class="text-5xl font-bold">
+            Exceptional Dental Care, on Your Terms
+          </h1>
+          <p class="py-6">
+            It is in the seeking of exceptional dental care, that discerning
+            patients will want to pay extra close attention to the dental
+            practitioner they choose. We enjoy a sterling reputation for the
+            incomparable quality care we give to our patients. Our dentist
+            Sheffield is not only highly proficient in a diverse range of
+            quality treatments and procedures, but also has a compassionate and
+            comforting bedside manner that patients find engaging and
+            encouraging. We believe that this success is due to our philosophy
+            which centres on the interests of our patients.
+          </p>
+          <button class="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExtraCare;
