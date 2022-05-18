ff581a631816ea67b6ebf8546ed23e7726c7ac56
meraj191-35-2735 <www.shamsad35-2735@diu.edu.bd>
2022-05-18 16:08:36 +0600

    homepage done

--- src/Pages/Home/Footer.js
+++ src/Pages/Home/Footer.js
@@ -1,30 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import footerBg from "../../assets/images/footer.png";
 
 const Footer = () => {
   return (
-    <footer class="footer p-10 bg-neutral text-neutral-content mt-20">
-      <div>
-        <span class="footer-title">Services</span>
-        <Link to="/about">Branding</Link>
-        <Link to="/about">Design</Link>
-        <Link to="/about">Marketing</Link>
-        <Link to="/about">Advertisement</Link>
+    <div>
+      <footer
+        style={{ background: `url(${footerBg})`, backgroundSize: "cover" }}
+        class="footer p-10 mt-20"
+      >
+        <div>
+          <span class="footer-title">Services</span>
+          <Link to="/about">Branding</Link>
+          <Link to="/about">Design</Link>
+          <Link to="/about">Marketing</Link>
+          <Link to="/about">Advertisement</Link>
+        </div>
+        <div>
+          <span class="footer-title">Company</span>
+          <Link to="/home">About us</Link>
+          <Link to="/home">Contact</Link>
+          <Link to="/home">Jobs</Link>
+          <Link to="/home">Press kit</Link>
+        </div>
+        <div>
+          <span class="footer-title">Legal</span>
+          <Link to="/contactus">Terms of use</Link>
+          <Link to="/contactus">Privacy policy</Link>
+          <Link to="/contactus">Cookie policy</Link>
+        </div>
+      </footer>
+      <div className="text-center pb-0 mb-0">
+        <p>Copyright © 2022 - All right reserved by Meraj</p>
       </div>
-      <div>
-        <span class="footer-title">Company</span>
-        <Link to="/home">About us</Link>
-        <Link to="/home">Contact</Link>
-        <Link to="/home">Jobs</Link>
-        <Link to="/home">Press kit</Link>
-      </div>
-      <div>
-        <span class="footer-title">Legal</span>
-        <Link to="/contactus">Terms of use</Link>
-        <Link to="/contactus">Privacy policy</Link>
-        <Link to="/contactus">Cookie policy</Link>
-      </div>
-    </footer>
+    </div>
   );
 };
 
